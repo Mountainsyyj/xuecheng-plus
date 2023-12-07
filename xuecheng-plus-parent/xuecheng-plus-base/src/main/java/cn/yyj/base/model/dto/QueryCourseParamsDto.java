ef2118b06e811c6fca8626ba1619a2c8f6f4d7f8
yyj <609082367@qq.com>
2023-12-07 19:32:53 +0800

    #init swagger config and data time format.

--- xuecheng-plus-parent/xuecheng-plus-base/src/main/java/cn/yyj/base/model/dto/QueryCourseParamsDto.java
+++ xuecheng-plus-parent/xuecheng-plus-base/src/main/java/cn/yyj/base/model/dto/QueryCourseParamsDto.java
@@ -1,14 +1,19 @@
 package cn.yyj.base.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class QueryCourseParamsDto {
 
+    @ApiModelProperty("审核状态")
     //审核状态
     private String auditStatus;
+
+    @ApiModelProperty("课程名称")
     //课程名称
     private String courseName;
+
     //发布状态
     private String publishStatus;
 
--- xuecheng-plus-parent/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/cn/yyj/ContentApplication.java
+++ xuecheng-plus-parent/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/cn/yyj/ContentApplication.java
@@ -1,9 +1,11 @@
 package cn.yyj;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2Doc
 public class ContentApplication {
     public static void main(String[] args) {
 
